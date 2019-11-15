280724b616b1a239d848aad1801648d214ad5391
Shannyn Telander <telander@bitwise.io>
2019-11-15 15:47:31 -0600

    Add TripleKeyHashAddresser
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- sdk/src/simple_state/addresser.rs
+++ sdk/src/simple_state/addresser.rs
@@ -98,3 +98,138 @@ impl Addresser<(String, String)> for DoubleKeyHashAddresser {
         keys.0.to_string() + "_" + &keys.1
     }
 }
+
+pub struct TripleKeyHashAddresser {
+    prefix: String,
+    first_hash_length: usize,
+    second_hash_length: usize,
+}
+
+impl TripleKeyHashAddresser {
+    pub fn new(
+        prefix: String,
+        first_hash_length: Option<usize>,
+        second_hash_length: Option<usize>,
+    ) -> TripleKeyHashAddresser {
+        let (first, second) =
+            calculate_hash_lengths(prefix.len(), first_hash_length, second_hash_length);
+        TripleKeyHashAddresser {
+            prefix,
+            first_hash_length: first,
+            second_hash_length: second,
+        }
+    }
+}
+
+impl Addresser<(String, String, String)> for TripleKeyHashAddresser {
+    fn compute(&self, keys: &(String, String, String)) -> Result<String, SimpleStateError> {
+        let hash_length = ADDRESS_LENGTH - self.prefix.len();
+        let last_hash_length = hash_length - (self.first_hash_length + self.second_hash_length);
+        if (self.prefix.len() + self.first_hash_length + self.second_hash_length + last_hash_length)
+            != ADDRESS_LENGTH
+        {
+            return Err(SimpleStateError::AddresserError(
+                "Incorrect hash length".to_string(),
+            ));
+        }
+
+        let first_hash = &hash(self.first_hash_length, &keys.0);
+        let second_hash = &hash(self.second_hash_length, &keys.1);
+        let third_hash = &hash(last_hash_length, &keys.2);
+
+        Ok(String::from(&self.prefix) + first_hash + second_hash + third_hash)
+    }
+
+    fn normalize(&self, keys: &(String, String, String)) -> String {
+        keys.0.to_string() + "_" + &keys.1 + "_" + &keys.2
+    }
+}
+
+// Used to calculate the lengths of the key hashes to be used to create an address by the
+// TripleKeyHashAddresser.
+fn calculate_hash_lengths(
+    prefix_length: usize,
+    first_length: Option<usize>,
+    second_length: Option<usize>,
+) -> (usize, usize) {
+    match (first_length, second_length) {
+        (Some(first), Some(second)) => (first, second),
+        (None, Some(second)) => (((ADDRESS_LENGTH - prefix_length - second) / 2), second),
+        (Some(first), None) => (first, ((ADDRESS_LENGTH - prefix_length - first) / 2)),
+        (None, None) => (
+            ((ADDRESS_LENGTH - prefix_length) / 3),
+            ((ADDRESS_LENGTH - prefix_length) / 3),
+        ),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    // Tests the calculate_hash_lengths function using several different custom first hash lengths
+    // and `None` for the second length.
+    fn test_calculate_hash_custom_first_length() {
+        let (first_length, second_length) = calculate_hash_lengths(6, Some(21), None);
+        assert_eq!(first_length, 21);
+        assert_eq!(second_length, (43 / 2));
+
+        let (first_length, second_length) = calculate_hash_lengths(6, Some(41), None);
+        assert_eq!(first_length, 41);
+        assert_eq!(second_length, (23 / 2));
+
+        let (first_length, second_length) = calculate_hash_lengths(6, Some(61), None);
+        assert_eq!(first_length, 61);
+        assert_eq!(second_length, (3 / 2));
+    }
+
+    #[test]
+    // Tests the calculate_hash_lengths function using `None` for the first hash length and several
+    // custom values for the second length.
+    fn test_calculate_hash_custom_second_length() {
+        let (first_length, second_length) = calculate_hash_lengths(6, None, Some(21));
+        assert_eq!(first_length, (43 / 2));
+        assert_eq!(second_length, 21);
+
+        let (first_length, second_length) = calculate_hash_lengths(6, None, Some(41));
+        assert_eq!(first_length, (23 / 2));
+        assert_eq!(second_length, 41);
+
+        let (first_length, second_length) = calculate_hash_lengths(6, None, Some(61));
+        assert_eq!(first_length, (3 / 2));
+        assert_eq!(second_length, 61);
+    }
+
+    #[test]
+    // Tests the calculate_hash_lengths function using several different custom hash lengths.
+    fn test_calculate_hash_custom_lengths() {
+        let (first_length, second_length) = calculate_hash_lengths(6, Some(42), Some(12));
+        assert_eq!(first_length, 42);
+        assert_eq!(second_length, 12);
+
+        let (first_length, second_length) = calculate_hash_lengths(6, Some(12), Some(42));
+        assert_eq!(first_length, 12);
+        assert_eq!(second_length, 42);
+
+        let (first_length, second_length) = calculate_hash_lengths(6, Some(20), Some(20));
+        assert_eq!(first_length, 20);
+        assert_eq!(second_length, 20);
+    }
+
+    #[test]
+    // Tests the calculate_hash_lengths function using `None` for both hash lengths.
+    fn test_calculate_hash_no_custom_lengths() {
+        let (first_length, second_length) = calculate_hash_lengths(6, None, None);
+        assert_eq!(first_length, 21);
+        assert_eq!(second_length, 21);
+
+        let (first_length, second_length) = calculate_hash_lengths(30, None, None);
+        assert_eq!(first_length, (40 / 3));
+        assert_eq!(second_length, (40 / 3));
+
+        let (first_length, second_length) = calculate_hash_lengths(50, None, None);
+        assert_eq!(first_length, (20 / 3));
+        assert_eq!(second_length, (20 / 3));
+    }
+}
