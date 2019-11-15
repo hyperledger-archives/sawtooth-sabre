848bd296d437b7be5a5de26ad7086a1e3cb4913b
Shannyn Telander <telander@bitwise.io>
2019-11-15 15:45:05 -0600

    Implement KeyHashAddresser
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- sdk/src/simple_state/addresser.rs
+++ sdk/src/simple_state/addresser.rs
@@ -14,6 +14,9 @@
 
 use crate::simple_state::error::SimpleStateError;
 
+use crypto::digest::Digest;
+use crypto::sha2::Sha512;
+
 pub const ADDRESS_LENGTH: usize = 70;
 
 pub trait Addresser<K> {
@@ -32,4 +35,32 @@ pub trait Addresser<K> {
     /// * `keys` - Contains natural keys
     ///
     fn normalize(&self, keys: &K) -> String;
+}
+
+fn hash(hash_length: usize, key: &str) -> String {
+    let mut sha = Sha512::new();
+    sha.input(key.as_bytes());
+    sha.result_str()[..hash_length].to_string()
+}
+
+pub struct KeyHashAddresser {
+    prefix: String,
+}
+
+impl KeyHashAddresser {
+    pub fn new(prefix: String) -> KeyHashAddresser {
+        KeyHashAddresser { prefix }
+    }
+}
+
+impl Addresser<String> for KeyHashAddresser {
+    fn compute(&self, keys: &String) -> Result<String, SimpleStateError> {
+        let hash_length = ADDRESS_LENGTH - self.prefix.len();
+
+        Ok(String::from(&self.prefix) + &hash(hash_length, keys))
+    }
+
+    fn normalize(&self, key: &String) -> String {
+        key.to_string()
+    }
 }
