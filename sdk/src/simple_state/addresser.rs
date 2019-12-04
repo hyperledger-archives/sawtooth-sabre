02383634d72b6a4ed36a0f5c5310eb6a6a46f068
Shannyn Telander <telander@bitwise.io>
2019-12-04 14:21:48 -0600

    Test Addresser implementations
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- sdk/src/simple_state/addresser.rs
+++ sdk/src/simple_state/addresser.rs
@@ -168,6 +168,179 @@ mod tests {
     use super::*;
 
     #[test]
+    // check that the KeyHashAddresser creates a valid radix address with the correct prefix
+    // and valid length
+    fn test_key_hash_addresser() {
+        let addresser = KeyHashAddresser::new("prefix".to_string());
+        let addr = addresser.compute(&"a".to_string()).unwrap();
+        assert_eq!(addr[..6], "prefix".to_string());
+        assert_eq!(addr.len(), 70);
+
+        let key_hash = hash(64, "a");
+        assert_eq!(addr[6..70], key_hash[..64]);
+
+        let normalized = addresser.normalize(&"b".to_string());
+        assert_eq!(normalized, "b".to_string());
+    }
+
+    #[test]
+    // check that the DoubleKeyHashAddresser creates a valid radix address with the correct prefix
+    // and valid default length, with a key represented as a tuple with two natural keys
+    fn test_double_key_default_length() {
+        let addresser = DoubleKeyHashAddresser::new("prefix".to_string(), None);
+        let key1 = "a";
+        let key1_hash = hash(32, key1);
+        let key2 = "b";
+        let key2_hash = hash(32, key2);
+
+        let addr = addresser
+            .compute(&(key1.to_string(), key2.to_string()))
+            .unwrap();
+        assert_eq!(addr[..6], "prefix".to_string());
+        assert_eq!(addr.len(), 70);
+
+        assert_eq!(addr[6..38], key1_hash[..32]);
+        assert_eq!(addr[38..], key2_hash[..32]);
+
+        let normalized = addresser.normalize(&(key1.to_string(), key2.to_string()));
+        assert_eq!(normalized, "a_b".to_string());
+    }
+
+    #[test]
+    // check that the DoubleKeyHashAddresser creates a valid radix address with the correct prefix
+    // and valid first hash length of 16, with a key represented as a tuple with two natural keys
+    fn test_double_key_custom_length() {
+        let addresser = DoubleKeyHashAddresser::new("prefix".to_string(), Some(16));
+        let key1 = "a";
+        let key1_hash = hash(16, key1);
+        let key2 = "b";
+        let key2_hash = hash(48, key2);
+
+        let addr = addresser
+            .compute(&(key1.to_string(), key2.to_string()))
+            .unwrap();
+        assert_eq!(addr[..6], "prefix".to_string());
+        assert_eq!(addr.len(), 70);
+
+        assert_eq!(addr[6..22], key1_hash[..16]);
+        assert_eq!(addr[22..], key2_hash[..48]);
+
+        let normalized = addresser.normalize(&(key1.to_string(), key2.to_string()));
+        assert_eq!(normalized, "a_b".to_string());
+    }
+
+    #[test]
+    // check that the TripleKeyHashAddresser creates a valid radix address with the correct prefix
+    // and valid default lengths, with a key represented as a tuple with three natural keys
+    fn test_triple_key_default_length() {
+        let addresser = TripleKeyHashAddresser::new("prefix".to_string(), None, None);
+        let key1 = "a";
+        let key1_hash = hash(21, key1);
+        let key2 = "b";
+        let key2_hash = hash(21, key2);
+        let key3 = "c";
+        let key3_hash = hash(22, key3);
+
+        let addr = addresser
+            .compute(&(key1.to_string(), key2.to_string(), key3.to_string()))
+            .unwrap();
+        assert_eq!(addr[..6], "prefix".to_string());
+        assert_eq!(addr.len(), 70);
+
+        assert_eq!(addr[6..27], key1_hash[..21]);
+        assert_eq!(addr[27..48], key2_hash[..21]);
+        assert_eq!(addr[48..], key3_hash[..22]);
+
+        let normalized =
+            addresser.normalize(&(key1.to_string(), key2.to_string(), key3.to_string()));
+        assert_eq!(normalized, "a_b_c".to_string());
+    }
+
+    #[test]
+    // check that the TripleKeyHashAddresser creates a valid radix address with the correct prefix
+    // and valid first hash length of 14 and second and third hash length of 25,
+    // with a key represented as a tuple with three natural keys
+    fn test_triple_key_custom_first_length() {
+        let addresser = TripleKeyHashAddresser::new("prefix".to_string(), Some(14), None);
+        let key1 = "a";
+        let key1_hash = hash(14, key1);
+        let key2 = "b";
+        let key2_hash = hash(25, key2);
+        let key3 = "c";
+        let key3_hash = hash(25, key3);
+
+        let addr = addresser
+            .compute(&(key1.to_string(), key2.to_string(), key3.to_string()))
+            .unwrap();
+        assert_eq!(addr[..6], "prefix".to_string());
+        assert_eq!(addr.len(), 70);
+
+        assert_eq!(addr[6..20], key1_hash[..14]);
+        assert_eq!(addr[20..45], key2_hash[..25]);
+        assert_eq!(addr[45..], key3_hash[..25]);
+
+        let normalized =
+            addresser.normalize(&(key1.to_string(), key2.to_string(), key3.to_string()));
+        assert_eq!(normalized, "a_b_c".to_string());
+    }
+
+    #[test]
+    // check that the TripleKeyHashAddresser creates a valid radix address with the correct prefix
+    // and valid first and last hash length of 25 and second hash length of 14,
+    // with a key represented as a tuple with three natural keys
+    fn test_triple_key_custom_second_length() {
+        let addresser = TripleKeyHashAddresser::new("prefix".to_string(), None, Some(14));
+        let key1 = "a";
+        let key1_hash = hash(25, key1);
+        let key2 = "b";
+        let key2_hash = hash(14, key2);
+        let key3 = "c";
+        let key3_hash = hash(25, key3);
+
+        let addr = addresser
+            .compute(&(key1.to_string(), key2.to_string(), key3.to_string()))
+            .unwrap();
+        assert_eq!(addr[..6], "prefix".to_string());
+        assert_eq!(addr.len(), 70);
+
+        assert_eq!(addr[6..31], key1_hash[..25]);
+        assert_eq!(addr[31..45], key2_hash[..14]);
+        assert_eq!(addr[45..], key3_hash[..25]);
+
+        let normalized =
+            addresser.normalize(&(key1.to_string(), key2.to_string(), key3.to_string()));
+        assert_eq!(normalized, "a_b_c".to_string());
+    }
+
+    #[test]
+    // check that the TripleKeyHashAddresser creates a valid radix address with the correct prefix
+    // and valid first and second length of 10 and last hash length of 44,
+    // with a key represented as a tuple with three natural keys
+    fn test_triple_key_custom_lengths() {
+        let addresser = TripleKeyHashAddresser::new("prefix".to_string(), Some(10), Some(10));
+        let key1 = "a";
+        let key1_hash = hash(10, key1);
+        let key2 = "b";
+        let key2_hash = hash(10, key2);
+        let key3 = "c";
+        let key3_hash = hash(44, key3);
+
+        let addr = addresser
+            .compute(&(key1.to_string(), key2.to_string(), key3.to_string()))
+            .unwrap();
+        assert_eq!(addr[..6], "prefix".to_string());
+        assert_eq!(addr.len(), 70);
+
+        assert_eq!(addr[6..16], key1_hash[..10]);
+        assert_eq!(addr[16..26], key2_hash[..10]);
+        assert_eq!(addr[26..], key3_hash[..44]);
+
+        let normalized =
+            addresser.normalize(&(key1.to_string(), key2.to_string(), key3.to_string()));
+        assert_eq!(normalized, "a_b_c".to_string());
+    }
+
+    #[test]
     // Tests the calculate_hash_lengths function using several different custom first hash lengths
     // and `None` for the second length.
     fn test_calculate_hash_custom_first_length() {
