6ad8cd57b59f387806f052c6dc2124b8984dd79a
Peter Schwarz <pschwarz@bitwise.io>
2021-12-14 11:39:30 -0600

    Switch simple logger to use UTC timestamps
    This change updates simple logger to be configured with utc timestamps.
    Local timestamps require a compiler flag, unsound_local_offset, which
    is required by a transient dependency.  The time crate (the transient
    dependency in question) describes this flag as untested, and it fails in
    certain environments, such as CI.
    
    See
    https://time-rs.github.io/internal-api/time/index.html#feature-flags
    for more information
    
    Signed-off-by: Peter Schwarz <pschwarz@bitwise.io>

--- tp/src/main.rs
+++ tp/src/main.rs
@@ -29,7 +29,10 @@ fn main() {
         (@arg verbose: -v --verbose +multiple
          "increase output verbosity"))
     .get_matches();
-    let logger = simple_logger::SimpleLogger::new();
+    let logger = simple_logger::SimpleLogger::new()
+        // Switch to UTC timestamps, as local timestamps are not stable, by default. They are only
+        // available if the compiler flag "unsound_local_offset" has been set.
+        .with_utc_timestamps();
     let logger = match matches.occurrences_of("verbose") {
         0 => logger.with_level(LevelFilter::Warn),
         1 => logger.with_level(LevelFilter::Info),
